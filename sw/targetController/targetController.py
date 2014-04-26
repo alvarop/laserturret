4fe98c94931dcc6e7f6c0eff0c3205fe0c625c12
Alvaro Prieto <apg88zx@gmail.com>
2014-04-26 14:02:40 -0700

    Added thread lock and input queue Made serial read thread a daemon so it doesn't hang on exit Added SIGINT handler so we can exit 'nicely' if necessary

--- sw/targetController/targetController.py
+++ sw/targetController/targetController.py
@@ -3,8 +3,17 @@
 import sys
 import io
 import threading
+import Queue
 import time
+import signal
 
+def signal_handler(signal, frame):
+	print('You pressed Ctrl+C!')
+	sys.exit(0)
+
+#
+# Read serial stream and add lines to shared queue
+#
 class serialReadThread(threading.Thread):
 	def __init__(self,device):
 		super(serialReadThread, self).__init__()
@@ -16,14 +25,36 @@ class serialReadThread(threading.Thread):
 	def run(self):
 		while self.running:
 			line = self.inStream.readline();
-			print line,
+			inQueueLock.acquire()
+			inQueue.put(line)
+			inQueueLock.release()
 
-readThread = serialReadThread("/dev/cu.usbmodem1421")
+if not len(sys.argv) > 1:
+	print "Usage: " + sys.argv[0] + " </dev/serialdevicename>"
+	sys.exit(0)
 
+print "Press Ctrl + C to exit"
+
+signal.signal(signal.SIGINT, signal_handler)
+
+inQueueLock = threading.Lock()
+inQueue = Queue.Queue()
+
+# Start readThread as daemon so it will automatically close on program exit
+readThread = serialReadThread(sys.argv[1])
+readThread.daemon = True
 readThread.start()
 
-time.sleep(10)
+while 1:
+	if not inQueue.empty():
+		inQueueLock.acquire()
+		line = inQueue.get()
+		inQueueLock.release()
 
-print "exiting"
-readThread.running = 0
+		# Process line here
+		print line
 
+	else:
+		# Sleep so CPU doesn't spin at 100%
+		# TODO - add nicer blocking
+		time.sleep(0.001)
