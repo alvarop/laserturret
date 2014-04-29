f47852bcbe9a60586add902d6fb80f3e934edc4d
kglowinski <kathryn.glowinski@gmail.com>
2014-04-29 15:42:39 -0700

    Adding command line arguments to the pyfile.

--- sw/turretVision/turret_vision.py
+++ sw/turretVision/turret_vision.py
@@ -1,4 +1,12 @@
 import io
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--serial", type=str, default='/dev/ttyACM0') 
+parser.add_argument("-c", "--camera", type=int, default=1) 
+parser.add_argument("--xoffset", type=int, default=10) 
+parser.add_argument("-c", "--yoffset", type=int, default=-12) 
+ARGS = parser.parse_args()
 
 WIDTH = 640
 HEIGHT = 480
@@ -8,7 +16,7 @@ MIN_RADIUS = 2
 
 MIN_VELOCITY = 11
 
-CONTROLFILE = io.open('./testfile.text', mode='wt')
+CONTROLFILE = io.open(ARGS.s, mode='wt')
 
 def move_x(deviation):
 
@@ -39,4 +47,8 @@ def move_y(deviation):
 
     CONTROLFILE.write("qik 1 mov %s \n" % velocity)
     print("qik 1 mov %s \n" % velocity)
+
+
+#--serial /dev/ttyACM0 —camera 1 --xOffset 10 --yOffset -12
+
 
