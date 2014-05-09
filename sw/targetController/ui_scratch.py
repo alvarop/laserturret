d241917a7a9e2b45c446b82246edbe3761d76aa3
kglowinski <kathryn.glowinski@gmail.com>
2014-05-09 09:36:18 -0700

    Adding scratch file will useful basic info for libavg. Should be a working program, that can be run to show how the basic UI element function.

---
+++ sw/targetController/ui_scratch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from libavg import avg
+from time import gmtime, strftime
+
+def change_text():
+    
+    #Note: circle node's apparently don't have a text item, so
+    #it doesn't do anything to the circle node. I suspect if ms
+    #were included in my time string, we would see it change much
+    #faster than 1 / s.
+    for item in ls:
+        item.text = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+
+player = avg.Player.get()
+
+canvas = player.createMainCanvas(size=(640,480))
+rootNode = canvas.getRootNode()
+
+bar = avg.WordsNode(pos=(10,10), font="arial", 
+    text="Hello World", parent=rootNode)
+word = avg.WordsNode(text='Hello libavg', pos=(10,30),
+    parent=rootNode)
+
+#This will execute the function for every frame of the monitor    
+player.setOnFrameHandler(change_text)
+
+#After 1 second, the thing will change, but only once.
+player.setTimeout(1000, change_text)
+
+#Every .2 s, will change.
+#player.setInterval(200, change_text)
+
+foo = avg.CircleNode(r=10, pos=(200,230), parent=rootNode)
+
+foo.color = "FF8000"
+
+ls = [word, foo, bar]
+
+player.play()
