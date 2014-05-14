6f1fe8ba051a4e642e1796d1c870c529a48ff5ca
Alvaro Prieto <alvaro@alvaro-portege.(none)>
2014-05-13 21:41:02 -0700

    Start creating shootingGallery object (so we can have more than one!)

--- sw/targetController/targetController.py
+++ sw/targetController/targetController.py
@@ -13,13 +13,58 @@ def signal_handler(signal, frame):
 	print "exiting"
 	sys.exit(0)
 
-class Target():
-	def __init__(self, targetID):
+class ShootingGallery():
+	def __init__(self, streamFileName, id):
+		stream = serial.Serial(streamFileName)
+
+		self.id = id
+		self.started = False
+
+		# Start readThread as daemon so it will automatically close on program exit
+		self.readThread = serialReadThread(stream, self.id)
+		self.readThread.daemon = True
+		self.readThread.start()
+
+		# Start writeThread as daemon so it will automatically close on program exit
+		self.writeThread = serialWriteThread(stream)
+		self.writeThread.daemon = True
+		self.writeThread.start()
+
+		self.targets = {}
+
+	def start(self):
+		print "starting gallery", str(self.id)
+
+		# Start scan for connected targets
+		self.write("init\n")
+
+		# Enable targets and wait for hits
+		galleries[0].writeThread.write("start\n")
+
+		while not self.started:
+			eventLock.wait(0.1)
+			eventLock.clear()
+
+		print "gallery started"
+
+	def configFromFile(self, configFile):
+		# read in a config file
+		config = open(configFile, 'r')
+		# read config lines
+		for line in config:
+			galleries[0].writeThread.write(line.strip() + "\n")
+
+	def write(self, command):
+		self.writeThread.write(command)
+
+class Target(object):
+	def __init__(self, targetID, gallery):
 		self.id = targetID
 		self.on = False
 		self.lock = threading.Lock()
+		self.gallery = gallery
 
-		writeThread.write("set " + str(self.id) + " 0\n")
+		self.gallery.write("set " + str(self.id) + " 0\n")
 
 	def hit(self):
 		self.on = False
@@ -27,29 +72,30 @@ class Target():
 	def enable(self):
 		self.lock.acquire()
 		self.on = True
-		writeThread.write("set " + str(self.id) + " 1\n")
+		self.gallery.write("set " + str(self.id) + " 1\n")
 		self.lock.release()
 
 	def disable(self):
 		self.lock.acquire()
 		self.on = False
-		writeThread.write("set " + str(self.id) + " 0\n")
+		self.gallery.write("set " + str(self.id) + " 0\n")
 		self.lock.release()
 
 #
 # Read serial stream and add lines to shared queue
 #
 class serialReadThread(threading.Thread):
-	def __init__(self, inStream):
+	def __init__(self, inStream, id):
 		super(serialReadThread, self).__init__()
 		self.stream = inStream
 		self.running = 1
+		self.id = id
 
 	def run(self):
 		while self.running:
 			line = self.stream.readline(100);
 			if line:
-				processLine(line, 0)
+				processLine(line, self.id)
 
 #
 # Write serial stream and add lines to shared queue
@@ -86,31 +132,31 @@ class serialWriteThread(threading.Thread):
 # Process lines coming from targetController via USB-serial link
 #
 def processLine(line, source):
-	global targets
 	args = line.split()
 
 	if len(args) > 1:
 		if args[1] == "connected":
 			targetID = int(args[0])
 			print "Target", targetID , "is connected"
-			targets[targetID] = Target(targetID)
-			targets[targetID].disable()
+			galleries[source].targets[targetID] = Target(targetID, galleries[source])
+			galleries[source].targets[targetID].disable()
 		elif args[1] == "hit":
 			targetID = int(args[0])
 			print "Target", targetID, "hit!"
-			if targetID in targets:
-				targets[targetID].hit()
+			if targetID in galleries[source].targets:
+				galleries[source].targets[targetID].hit()
 				while True:
-					currentTarget = randint(0, len(targets) - 1)
-					if currentTarget in targets:
+					currentTarget = randint(0, len(galleries[source].targets) - 1)
+					if currentTarget in galleries[source].targets:
 						break 
-				targets[currentTarget].enable()
+				galleries[source].targets[currentTarget].enable()
 		elif args[1] == "started":
+			galleries[source].started = True
 			while True:
-				currentTarget = randint(0, len(targets) - 1)
-				if currentTarget in targets:
+				currentTarget = randint(0, len(galleries[source].targets) - 1)
+				if currentTarget in galleries[source].targets:
 					break
-			targets[currentTarget].enable()
+			galleries[source].targets[currentTarget].enable()
 		else:
 			print "controller: ", line,
 
@@ -122,11 +168,11 @@ def processLine(line, source):
 # If they're all hit, exit
 #
 def checkTargets():
-	if(len(targets) > 0):
+	if(len(galleries[0].targets) > 0):
 		notDone = 0
 
-		for key in targets.keys():
-			if targets[key] == True:
+		for key in galleries[0].targets.keys():
+			if galleries[0].targets[key].on == True:
 				notDone = 1
 
 		if notDone == 0:
@@ -147,40 +193,17 @@ print "Press Ctrl + C to exit"
 
 eventLock = threading.Event() # Used to block main thread while waiting for events
 
-targets = {}
 done = 0
 currentTarget = 0
-started = False
 
-stream = serial.Serial(sys.argv[1])
-
-# Start readThread as daemon so it will automatically close on program exit
-readThread = serialReadThread(stream)
-readThread.daemon = True
-readThread.start()
-
-# Start writeThread as daemon so it will automatically close on program exit
-writeThread = serialWriteThread(stream)
-writeThread.daemon = True
-writeThread.start()
-
-# Start scan for connected targets
-writeThread.write("init\n")
+galleries = {}
+galleries[0] = ShootingGallery(sys.argv[1], 0)
+galleries[0].start()
 
 # read in a config file
 if len(sys.argv) > 2:
-	configFile = sys.argv[2].strip()
-	config = open(configFile, 'r')
-	# read config lines
-	for line in config:
-		writeThread.write(line.strip() + "\n")
+	galleries[0].configFromFile(sys.argv[2].strip())
 
-# Enable targets and wait for hits
-writeThread.write("start\n")
-
-while not started:
-	eventLock.wait(0.1)
-	eventLock.clear()
 
 startTime = datetime.now()
 
