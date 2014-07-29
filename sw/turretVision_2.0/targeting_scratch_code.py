27699c7e589ecd2682b3411cc02b19e889537066
Kathryn <kathryn@NCP-Castor.Stanford.EDU>
2014-07-29 16:01:43 -0700

    Fixed scratch code so it detects previously undetectable circles. Btu very simplified code. Borks on images with overlapping targets.

--- sw/turretVision_2.0/targeting_scratch_code.py
+++ sw/turretVision_2.0/targeting_scratch_code.py
@@ -4,9 +4,11 @@ from SimpleCV.Shell import plot
 
 def main():
     
-    display = scv.Display()
+    display = scv.Display(resolution=(800,600))
+    
+    no_circle_uri = './two_circle_single_detect.png'
 
-    no_circle_uri = './single_circle_no_detect.png'
+    #no_circle_uri = './single_circle_no_detect.png'
     '''detect_circle_uri = './single_circle_detetcted.png'
     two_circle_one_detect_uri = './two_circle_single_detect.png.'
 
@@ -24,23 +26,33 @@ def main():
 
     pared_image = no_circle.hueDistance(123)
 
-    blobs = pared_image.findBlobs(minsize=30)
+    blobs = pared_image.findBlobs(minsize=200)
+    #.2 is the tolerance for matching a perfect circle. Higher the number, the more things
+    #the filter will allow.
+    blobs = blobs.filter([b.isCircle(0.2) for b in blobs])
 
     for b in blobs:
         #print "lookin at blob"
         #if b.isCircle(tolerance=.1):
         #    print "found circle!"    
-        no_circle_out.drawRectangle(b.x, b.y, 2, 2, scv.Color.YELLOW)
+        no_circle_out.drawCircle((b.x, b.y), b.radius(), scv.Color.YELLOW, 8)
         #    print ("Circle X: %s, Y: %s, Radius: %s" % (b.x, b.y, b.radius()))
 
-    img = no_circle_out
+    no_circle_out.save('./testing_prev_undetected_left.png') 
+
+    normal_disp = True
 
     while display.isNotDone():
-        img.show()
 
         if display.mouseRight:
             break
 
         if display.mouseLeft:
-            img = pared_image
+            normal_disp = not(normal_disp)
+
+        if normal_disp:
+            no_circle_out.show()
+        else:
+            pared_image.show()
 main()
+
