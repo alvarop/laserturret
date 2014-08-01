c988450c817e3d17b48c942c4b97aeb42bea6c1b
Kathryn <kathryn@NCP-Castor.Stanford.EDU>
2014-07-31 18:04:29 -0700

    Overhauling start. Going to use segmentation for tracking the difference between images.

--- sw/turretVision_2.0/turret_vision.py
+++ sw/turretVision_2.0/turret_vision.py
@@ -2,49 +2,58 @@
 
 import datetime
 import os
-import SimpleCV
+import SimpleCV as scv
 import argparse as arg
+import numpy as np
 
+display = scv.Display(resolution=(960,720))
+cam = scv.Camera(1, {"height": 768, "width": 1024})
+#Figure out what the number param is.
+rs = scv.RunningSegmentation(0.5)
 
-display = SimpleCV.Display()
-cam = SimpleCV.Camera(1, {"height": 1000, "width": 1000})
 normalDisplay = True
 
-
+flow = np.empty((1024,768)).fill(-1)
 
 while display.isNotDone():
     
     if display.mouseRight:
         break
-    '''
-    img = cam.getImage()
-    circles = img.findCircle()
-
-    try:
-        for c in circles:
-            img.draw(color=SimpleCV.Color.RED, width=5)
-    except TypeError:
-        continue 
-
-    img.show()
-    '''
     if display.mouseLeft:
 		normalDisplay = not(normalDisplay)
 		print "Display Mode:", "Normal" if normalDisplay else "Segmented" 
-        
+    
     img = cam.getImage().flipHorizontal()
-    dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2)
-    segmented = dist.stretch(200,255)
-    blobs = segmented.findBlobs()
-    if blobs:
-        circles = blobs.filter([b.isCircle(0.2) for b in blobs])
-        if circles:
-            img.drawCircle((circles[-1].x, circles[-1].y), circles[-1].radius(),SimpleCV.Color.BLUE,3)
-    if normalDisplay:
-        img.show()
+
+    if (rs.isReady()):
+        img = rs.getSegmentedImage(False) 
+        segmented = (img * 2 - 20).binarize(127).erode(1).invert()
+        blobs = segmented.findBlobs(minsize=40, maxsize=7000)
+
+        #If a blob was found, sort based on distance from the coordinate where we
+        #last sent the laser. For now, will use the default of the center.
+        if blobs:
+            
+            blobs = blobs.sortDistance()
+            b_x = blobs[0].minRectX()
+            b_y = blobs[0].minRectY()
+
+            #Here we do laser things to direct it to shoot at the current x and y.
+            #Likely will have to involve math.
+            img.dl().circle((b_x, b_y), scv.Color.RED, width=3)
+
     else:
-        #segmented.show()
+        pass
+
+
+
+    if normalDisplay:
+        img.save(display)
+    else:
+        '''
+        This code will allow you to save files by left clicking.
         dt = str(datetime.datetime.now())
         dt = dt.replace(' ', '_')
-        img.save('./circle_capture_' + dt + '.png')
+        img.save('./circle_capture_' + dt + '.png')'''
+        segmented.save(display)
         normalDisplay = not(normalDisplay)
