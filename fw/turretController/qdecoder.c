fe16e99d32efb9401037db8dd854d09bd1341168
Alvaro Prieto <apg88zx@gmail.com>
2014-08-02 08:48:17 -0700

    Adding second channel

--- fw/turretController/qdecoder.c
+++ fw/turretController/qdecoder.c
@@ -2,7 +2,8 @@
 #include "stm32f4xx_conf.h"
 #include "stm32f4xx.h"
 
-static int16_t oldValue;
+static int16_t oldX;
+static int16_t oldY;
 
 void qdecoderInit() {
 
@@ -15,7 +16,7 @@ void qdecoderInit() {
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6| GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 	
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_TIM8);
@@ -35,6 +36,35 @@ void qdecoderInit() {
 	/* TIM8 counter enable */
 	TIM_Cmd(TIM8, ENABLE);
 
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_TIM4);
+	
+	TIM_TimeBaseStructure.TIM_Period = 0xffff;
+	TIM_TimeBaseStructure.TIM_Prescaler = 0;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+	
+	/* Configure the timer */
+	TIM_EncoderInterfaceConfig(TIM4, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
+	
+	/* TIM4 counter enable */
+	TIM_Cmd(TIM4, ENABLE);
+
+
 }
 
 void qdecoderReset() {
@@ -43,10 +73,22 @@ void qdecoderReset() {
 }
 
 void qdecoderProcess() {
-	int16_t newValue = TIM_GetCounter(TIM8);
-	if(newValue != oldValue) {
-		oldValue = newValue;
-		printf("%d\n", newValue);
+	int16_t newX = TIM_GetCounter(TIM8);
+	int16_t newY = TIM_GetCounter(TIM4);
+	int32_t newValue = 0;
+
+	if(newX != oldX) {
+		oldX = newX;
+		newValue = 1;
+	}
+
+	if(newY != oldY) {
+		oldY = newY;
+		newValue = 1;
+	}
+
+	if(newValue) {
+		printf("%d, %d\n", oldX, oldY);
 	}
 
 }
