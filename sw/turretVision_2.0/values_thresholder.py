61fa215b9eb3af992f183be4d04478dfdce67380
kathryn <kathryn@localhost.localdomain>
2014-08-02 21:39:53 -0700

    At least it works now? Though, doesn't accept negative beta values. We'll roll with it.

--- sw/turretVision_2.0/values_thresholder.py
+++ sw/turretVision_2.0/values_thresholder.py
@@ -9,7 +9,7 @@ normalDisplay = True
 #Setting up the parts themselves
 cv2.namedWindow('image')
 cv2.createTrackbar('Contrast', 'image', 1,3, nothing)
-cv2.createTrackbar('Brightness(-200)', 'image', 50, 100, nothing) 
+cv2.createTrackbar('Brightness', 'image', 50, 100, nothing) 
 
 # create switch for ON/OFF functionality
 switch = '0 : NORM \n1 : SEG'
@@ -29,18 +29,25 @@ def main():
         _, img = cap.read()
 
         alpha = cv2.getTrackbarPos('Contrast', 'image')
+        a_array = np.empty_like(img)
+        a_array.fill(alpha)
         #Sliders can't be lower than 0, so starting at 50, then subtracting
         beta = cv2.getTrackbarPos('Brightness', 'image') - 50
+        print beta
+        b_array = np.empty_like(img)
+        b_array.fill(beta)
 
         toggle = cv2.getTrackbarPos(switch, 'image')
         segmented = False if toggle == 0 else True
         
-        num_temp = np.float64(img)
-        img = num_temp * alpha + beta
+        mul_img = cv2.multiply(img, a_array) 
+        #trans_img = cv2.add(mul_img, b_array)
+        trans_img = (alpha * img) - beta       
+
         if segmented:
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = cv2.cvtColor(trans_img, cv2.COLOR_BGR2GRAY)
             _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
             cv2.imshow('image', binary)
         else:
-            cv2.imshow('image', img)
+            cv2.imshow('image', trans_img)
 main()   
