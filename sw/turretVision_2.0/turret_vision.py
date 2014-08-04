ffda311160cc8094171177400c63f7d798a9b112
kathryn <kathryn@localhost.localdomain>
2014-08-03 19:22:09 -0700

    Starting again! One more time. Going back to moving camera, this is finding circles, then trying to guess where the next one will be.

--- sw/turretVision_2.0/turret_vision.py
+++ sw/turretVision_2.0/turret_vision.py
@@ -5,55 +5,97 @@ import os
 import SimpleCV as scv
 import argparse as arg
 import numpy as np
+import sys
+
+from math import sqrt
 
 display = scv.Display(resolution=(960,720))
 cam = scv.Camera(1, {"height": 768, "width": 1024})
-#Figure out what the number param is.
-rs = scv.RunningSegmentation(0.5)
-
 normalDisplay = True
 
-flow = np.empty((1024,768)).fill(-1)
+prev_coords = [(0,0)]
 
-while display.isNotDone():
+def find_shortest_distances(curr):
+    '''Naively assume that for a single coord, shortest distance
+    will be a 1-to-1 for all in both curr set and prev set.'''
+    master_set = []
     
-    if display.mouseRight:
-        break
-    if display.mouseLeft:
-		normalDisplay = not(normalDisplay)
-		print "Display Mode:", "Normal" if normalDisplay else "Segmented" 
+    for x,y in curr:
+        print "X:%s,Y:%s, prev:%s" % (x, y, prev_coords)  
+        best_dist = sys.maxint
+        best_pair = None    
+
+        for p_x, p_y in prev_coords:
+            print "Are getting into 0,0 look."
+            print "P_x: %s, p_y: %s" % (p_x, p_y)
+            dist = sqrt((x-p_x)**2 + (y-p_y)**2)
+            print "Dist: %s" % dist 
+            if dist < best_dist: 
+                best_dist = dist
+                best_pair = p_x, p_y
+        #Associate current x,y to optimal distance pair 
+        master_set.append([best_pair, (x,y)])
+    
+    return master_set
+
+def anticipate_the_future(matched_pairs):
+
+    print "Matched: %s" % matched_pairs
+    future_locs = []
+
+    for coord_set in matched_pairs:
+    
+        first_x, first_y = coord_set[0]
+        sec_x, sec_y = coord_set[1]
+
+        new_x = first_x + (sec_x-first_x)
+        new_y = first_y + (sec_y-first_y)
+
+        future_locs.append([(sec_x,sec_y), (new_x, new_y)])
+   
+    return future_locs 
+
+print "Prev coords: %s" % prev_coords        
+while display.isNotDone():
     
     img = cam.getImage().flipHorizontal()
 
-    if (rs.isReady()):
-        img = rs.getSegmentedImage(False) 
-        segmented = (img * 2 - 20).binarize(127).erode(1).invert()
-        blobs = segmented.findBlobs(minsize=40, maxsize=7000)
+    if display.mouseRight:
+        break
+    if display.mouseLeft:
+        normalDisplay = not(normalDisplay)
 
-        #If a blob was found, sort based on distance from the coordinate where we
-        #last sent the laser. For now, will use the default of the center.
-        if blobs:
-            
-            blobs = blobs.sortDistance()
-            b_x = blobs[0].minRectX()
-            b_y = blobs[0].minRectY()
+    segmented = img.colorDistance(scv.Color.WHITE).dilate(2).binarize(25)
+    #segmented = np.where(segmented < 200, 0, segmented)
+    blobs = segmented.findBlobs(minsize=200, maxsize=7000)
 
-            #Here we do laser things to direct it to shoot at the current x and y.
-            #Likely will have to involve math.
-            img.dl().circle((b_x, b_y), scv.Color.RED, width=3)
+    if blobs:
+        
+        circles = filter(lambda b: b.isCircle(0.4), blobs)
+        curr_coords = map(lambda b: (b.x, b.y), circles)
+        for b in circles:
+            b.drawOutline(scv.Color.RED, width=4, layer= img.dl())
+            b.drawOutline(scv.Color.RED, width=4, layer= segmented.dl())
+        
+        matched_pairs = find_shortest_distances(curr_coords)
+        prev_coords = curr_coords[:]
+        
+        if circles:
+            future_locs = anticipate_the_future(matched_pairs)  
+     
+            for coords in future_locs:
+                current = coords[0]
+                future_x, future_y = coords[1] 
+                
+                future_x = future_x * 2 if future_x * 2 < 960 else 959
+                future_y = future_y * 2 if future_y* 2 < 720 else 719
 
-    else:
-        pass
-
-
+                img.dl().line(current, (future_x, future_y), scv.Color.LIME, 4)
 
     if normalDisplay:
-        img.save(display)
+        img.show()
     else:
-        '''
-        This code will allow you to save files by left clicking.
-        dt = str(datetime.datetime.now())
-        dt = dt.replace(' ', '_')
-        img.save('./circle_capture_' + dt + '.png')'''
-        segmented.save(display)
-        normalDisplay = not(normalDisplay)
+        segmented.show()
+    
+
+ 
