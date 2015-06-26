f8de0667fc4fd5c10ec564ffc315a14ae3cfc77e
Alvaro Prieto <apg88zx@gmail.com>
2015-06-25 18:47:13 -0700

    Better attempt at inverse lookup, but still freaks out when selected pixel is between two known points

--- sw/galvoVision/lookupTest.py
+++ sw/galvoVision/lookupTest.py
@@ -37,11 +37,49 @@ def getLaserPos(pixelX, pixelY):
     sortedDistTable = sorted(distTable, key = lambda x:x[1])
 
     # Show the four closest calibration points
+    # Get the sum of all the distances
     newImg = copy.copy(img)
+    dSum = 0.0
     for index in range(4):
         _,_, x, y = dotTable[sortedDistTable[index][0]]
-        cv2.circle(newImg, (x, y), 10, [0,255,0])
-        print x,y
+        cv2.circle(newImg, (x, y), 10, [0,255-int(sortedDistTable[index][1]*4),0])
+        print x,y, sortedDistTable[index][1],1
+
+        dSum += sortedDistTable[index][1],1
+
+    print "dSum", dSum
+
+    # Now take the sum of 1/(dist/SUM(distances))
+    # Because we want the closest distance to have a higher weight
+    idSum = 0.0
+    nDist = []
+    for index in range(4):
+         nDist.append(1/sortedDistTable[index][1],1)
+         print nDist[index]
+         idSum += nDist[index]
+
+    newLaserX = 0
+    newLaserY = 0
+    newX = 0
+    newY = 0
+
+    print "idSum", idSum
+
+    # Now compute new x,y position using weighted averages
+    for index in range(4):
+        dist = sortedDistTable[index][1]
+        laserX,laserY, x, y = dotTable[sortedDistTable[index][0]]
+
+        print dist, idSum, nDist[index], (nDist[index]) / idSum # <--- how do we make the shorter one be weighed higher (but sum of all weights add up to 1)?
+
+        newX += x * nDist[index] / idSum
+        newY += y * nDist[index] / idSum
+        # newLaserX += 
+
+    newX = int(newX)
+    newY = int(newY)
+    print newX, newY
+    cv2.circle(newImg, (newX, newY), 5, [255,255,255])
 
     cv2.imshow("image", newImg)
 
