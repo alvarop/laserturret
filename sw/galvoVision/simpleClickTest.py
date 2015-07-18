7010fd7a445495f2a09021f519833af41a3af60a
Alvaro Prieto <apg88zx@gmail.com>
2015-07-17 18:04:07 -0700

    Crop image so we only see what we can shoot

--- sw/galvoVision/simpleClickTest.py
+++ sw/galvoVision/simpleClickTest.py
@@ -20,6 +20,9 @@ dotTable = []
 laserX = 0
 laserY = 0
 
+# Default to 1080p image
+imgBounds = (0,0,1920,1080) 
+
 def nothing(x):
     pass
 
@@ -62,6 +65,51 @@ class cameraReadThread(threading.Thread):
         self.frameReady = False
         return self.frame
 
+def getPointBounds(pointList, frame = (0,0,1920,1080), margin = 0):
+    minX = frame[2]
+    maxX = frame[0]
+    minY = frame[3]
+    maxY = frame[1]
+
+    for point in pointList:
+        x = point[2]
+        y = point[3]
+
+        if x > maxX:
+            maxX = x
+        if x < minX:
+            minX = x
+
+        if y > maxY:
+            maxY = y
+        if y < minY:
+            minY = y
+
+    # 
+    # Add in margin if necessary
+    # 
+    minX -= margin
+    maxX += margin
+
+    minY -= margin
+    maxY += margin    
+
+    # 
+    # Make sure we don't go out of bounds
+    # 
+    if minX < frame[0]:
+        minX = frame[0]
+
+    if maxX > frame[2]:
+        maxX = frame[2]
+
+    if minY < frame[1]:
+        minY = frame[1]
+
+    if maxY > frame[3]:
+        maxY = frame[3]
+
+    return (minX, minY, maxX, maxY)
 
 def setLaserState(state):
     if(state):
@@ -76,9 +124,13 @@ def setLaserPos(x, y):
 def mouseClick(event, x, y, flags, param):
     global laserX
     global laserY
-
+    global imgBounds
+    
     if event == cv2.EVENT_LBUTTONDOWN:
         print("Mouse clicked(" + str(x) + ", " + str(y) +")")
+        x += imgBounds[0]
+        y += imgBounds[1]
+        print("Adjusted for frame(" + str(x) + ", " + str(y) +")")
         laserPoint = getLaserPos(x, y, 1)
         laserX = laserPoint[0]
         laserY = laserPoint[1]
@@ -213,7 +265,14 @@ with open('dotTable.csv', 'rb') as csvfile:
         if int(row[2]) > 0 and int(row[3]) > 0:
             dotTable.append([int(row[0]), int(row[1]), int(row[2]), int(row[3])])
 
-cam = 0
+# Get image margins from dotTable
+# No need to display what we can't shoot
+# TODO - pass as parameter?
+imgBounds = getPointBounds(dotTable, margin=25)
+
+print('New image bounds: (' + str(imgBounds[0]) + ',' +str(imgBounds[1]) + ',' +str(imgBounds[2]) + ',' + str(imgBounds[3]) + ')')
+
+cam = 1
 cameraThread = cameraReadThread(cam)
 cameraThread.daemon = True
 cameraThread.start()
@@ -228,10 +287,9 @@ cv2.setMouseCallback("image", mouseClick)
 setLaserState(False)
 while running:
     image = cameraThread.getFrame()
-    cv2.circle(image, (laserX, laserY), 5, [0,255,0])
-    # TODO - only show relevant part of the picture here
-    # will have to adjust mouse, but if we can't shoot it, why even look at it!?
-    cv2.imshow("image", image)
+    # cv2.circle(image, (laserX, laserY), 5, [0,255,0])
+
+    cv2.imshow("image", image[imgBounds[1]:imgBounds[3], imgBounds[0]:imgBounds[2]])
     k = cv2.waitKey(1)
     if k == 27:
         running = False
