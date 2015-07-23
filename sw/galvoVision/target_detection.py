Fixed masking, targets now added to alive or dead lists.

sw/galvoVision/target_detection.py
@@ -1,6 +1,34 @@
-import cv2
 import argparse
+import threading
+import time
+
+import cv2
 import numpy as np
+
+class cameraReadThread(threading.Thread):
+    def __init__(self, cam):
+        super(cameraReadThread, self).__init__()
+        self.cap = cv2.VideoCapture(cam)
+        # self.cap.set(3, 1920)
+        # self.cap.set(4, 1080)
+        w = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        h = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        print("Resolution: (" + str(int(w)) + "," + str(int(h)) + ")")
+
+        self.running = 1
+        self.frameReady = False
+
+    def run(self):
+        while self.running:
+            _, self.frame = self.cap.read()
+            self.frameReady = True
+
+    def getFrame(self):
+        while(self.frameReady == False):
+            time.sleep(0.001)
+
+        self.frameReady = False
+        return self.frame
 
 def parse_args():
    
@@ -16,20 +44,26 @@ def parse_args():
 def main():
 
     args = parse_args()
+
+    cam = args.v_input
+    cameraThread = cameraReadThread(cam)
+    cameraThread.daemon = True
+
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.namedWindow('masked', cv2.WINDOW_NORMAL)
     cv2.namedWindow('contours', cv2.WINDOW_NORMAL)
 
-    cap = cv2.VideoCapture(args.v_input)
     fgbg = cv2.createBackgroundSubtractorMOG2()
-    # fgbg = cv2.createBackgroundSubtractorKNN()
+    #fgbg = cv2.createBackgroundSubtractorKNN()
 
+    cameraThread.start()
+    LOCKED = False
 
     print args
 
     while (True):
 
-        _, frame = cap.read()
+        frame = cameraThread.getFrame()
         mask = fgbg.apply(frame)
         # ret,thresh = cv2.threshold(mog_sub,127,255,0)
 
@@ -39,13 +73,32 @@ def main():
         # if args.template:
         #     use_match_detection(frame, args.template)
         # else:
-        use_feature_contours(mask, frame, 6)
+        contours = use_feature_contours(mask)
+
+        n_contours, LOCKED = get_n_contours(contours, 1)
+        if LOCKED and n_contours:
+            l_bound, r_bound, u_bound, b_bound = get_mask_coords(n_contours)
+            print (l_bound, r_bound, u_bound, b_bound)
+        # if n_contours:
+        #     live_trgs, dead_trgs = racial_profile(frame, n_contours)
+
+        draw(frame, n_contours)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+def get_mask_coords(n_contours):
 
-def use_feature_contours(mask, frame, n):
+    print "Running get coords on contour of %s size" % cv2.contourArea(n_contours[0])
+
+    leftmost = min([tuple(cnt[cnt[:,:,0].argmin()][0])[0] for cnt in n_contours])
+    rightmost = max([tuple(cnt[cnt[:,:,0].argmax()][0])[0] for cnt in n_contours])
+    topmost = min([tuple(cnt[cnt[:,:,1].argmin()][0])[1] for cnt in n_contours])
+    bottommost = max([tuple(cnt[cnt[:,:,1].argmax()][0])[1] for cnt in n_contours])
+
+    return leftmost, rightmost, topmost, bottommost
+
+def use_feature_contours(mask):
 
     cp = mask.copy()
     cv2.imshow('masked', cp)
@@ -53,28 +106,26 @@ def use_feature_contours(mask, frame, n):
         cv2.findContours(cp, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     contours = [cv2.approxPolyDP(cnt, 3, True) for cnt in contours]
 
-    n_contours = get_n_contours(contours, n)
-    if n_contours:
-        live_trgs, dead_trgs = racial_profile(frame, n_contours)
-
-    draw(frame, n_contours)
-
+    return contours
 
 def racial_profile(source_img, contours):
     live, dead = [], []
 
     for h, cnt in enumerate(contours):
-        mask = np.zeros(source_img.shape, np.uint8)
+        mask = np.zeros(source_img.shape[:2], np.uint8)
         cv2.drawContours(mask, [cnt], 0, 255, -1)
-        print mask
-        print mask.dtype
+
         mean = cv2.mean(source_img, mask=mask)
 
-        # Logic will live here for what color is blue vs white, and where to go.
-        live.append(mean)
+        if mean[0] > 110 and mean[1] > 110:
+            print "Trg %s is dead" % h
+            dead.append(cnt)
+        else:
+            print "Trg %s is live" % h
+            live.append(cnt)
 
-    # print live
     return live, dead
+
 
 def draw(img_out, contours):
     """
@@ -98,15 +149,29 @@ def get_n_contours(all_contours, n):
 
     max_indices = np.argsort(-areas)[:n]
     max_contours = [all_contours[idx] for idx in max_indices]
-    return max_contours
+
+    locked = all([cv2.contourArea(x) > 100 for x in max_contours])
+
+    # def center(cnt):
+    #     M = cv2.moments(cnt)
+    #     centroid_x = int(M['m10']/M['m00'])
+    #     centroid_y = int(M['m01']/M['m00'])
+    #     return centroid_x, centroid_y
+    # try:
+    #     print [center(x) for x in max_contours]
+    # except ZeroDivisionError:
+    #     pass
+
+    return max_contours, locked
 
 
-def draw_bounding_rect(out_img, contours):
+def draw_bounding_rect(out_img, contours, filled=False):
 
     for c in contours:
         # Now draw bounding box
         x, y, w, h = cv2.boundingRect(c)
-        cv2.rectangle(out_img,(x,y),(x+w,y+h),(0,255,0),2)
+        thickness = -1 if filled else 2
+        cv2.rectangle(out_img,(x,y),(x+w,y+h),(0,255,0), thickness)
 
 def draw_bounding_circle(out_img, contours):
 
@@ -118,7 +183,7 @@ def draw_bounding_circle(out_img, contours):
         cv2.circle(out_img, center, radius, (0, 0, 255), 3)
 
 #def use_match_detection(frame, template):
-#     gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+#     gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)q
 #     template = cv2.imread(template, 0)
 #     w, h = template.shape[::-1]
 #