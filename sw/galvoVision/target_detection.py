7bb3569934dca0fa9e1001304edce99e24f8a958
Kathryn <kathryn.glowinski@gmail.com>
2015-07-25 22:49:27 -0400

    Future x,y anticipation works, though line drawing into the future behaves strangely.

--- sw/galvoVision/target_detection.py
+++ sw/galvoVision/target_detection.py
@@ -1,9 +1,13 @@
 import argparse
+from math import sqrt
 import threading
 import time
+from sys import maxint
+
 
 import cv2
 import numpy as np
+
 
 class cameraReadThread(threading.Thread):
     def __init__(self, cam):
@@ -58,31 +62,39 @@ def main():
 
     cameraThread.start()
     LOCKED = False
+    prev_contour_set = None
 
     print args
 
     while (True):
 
+        future_pairs = []
+        # if LOCKED:
+        #     frame = mask_original_frame(cameraThread.getFrame(), (l_bound, r_bound, u_bound, b_bound))
+        # else:
+        #     frame = cameraThread.getFrame()
         frame = cameraThread.getFrame()
-        mask = fgbg.apply(frame)
-        # ret,thresh = cv2.threshold(mog_sub,127,255,0)
+        movement_mask = fgbg.apply(frame)
 
-        # vis = np.concatenate((mog_sub, thresh), axis=0)
         cv2.imshow('image', frame)
 
-        # if args.template:
-        #     use_match_detection(frame, args.template)
-        # else:
-        contours = use_feature_contours(mask)
+        contours = use_feature_contours(movement_mask)
 
         n_contours, LOCKED = get_n_contours(contours, 1)
         if LOCKED and n_contours:
-            l_bound, r_bound, u_bound, b_bound = get_mask_coords(n_contours)
-            print (l_bound, r_bound, u_bound, b_bound)
+            if prev_contour_set:
+                matched_coords = find_shortest_distances(prev_contour_set, n_contours)
+                future_pairs = anticipate_the_future(matched_coords)
+
+            prev_contour_set = n_contours
+
+            # #Get extents of current contour set
+            # l_bound, r_bound, u_bound, b_bound = get_mask_coords(n_contours)
+            # print (l_bound, r_bound, u_bound, b_bound)
         # if n_contours:
         #     live_trgs, dead_trgs = racial_profile(frame, n_contours)
 
-        draw(frame, n_contours)
+        draw(frame, n_contours, future_pairs)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -127,7 +139,7 @@ def racial_profile(source_img, contours):
     return live, dead
 
 
-def draw(img_out, contours):
+def draw(img_out, contours, future_pairs):
     """
     DRAW THE THING. And any additional things you want.
     """
@@ -136,6 +148,8 @@ def draw(img_out, contours):
     cv2.drawContours(vis, contours, -1, (128,255,255), 3, cv2.LINE_AA)
 
     draw_bounding_rect(vis, contours)
+    if future_pairs:
+        draw_the_future(vis, future_pairs)
     # draw_bounding_circle(vis, contours)
 
     cv2.imshow('contours', vis)
@@ -152,17 +166,15 @@ def get_n_contours(all_contours, n):
 
     locked = all([cv2.contourArea(x) > 100 for x in max_contours])
 
-    # def center(cnt):
-    #     M = cv2.moments(cnt)
-    #     centroid_x = int(M['m10']/M['m00'])
-    #     centroid_y = int(M['m01']/M['m00'])
-    #     return centroid_x, centroid_y
-    # try:
-    #     print [center(x) for x in max_contours]
-    # except ZeroDivisionError:
-    #     pass
-
     return max_contours, locked
+
+
+def mask_original_frame(frame, coords):
+    mask = np.zeros(frame.shape[:2], np.uint8)
+
+    # Coords are L, R, T, B
+    cv2.rectangle(mask, (coords[0], coords[2]), (coords[1], coords[3]), 255, -1)
+    return cv2.bitwise_and(frame, frame, mask=mask)
 
 
 def draw_bounding_rect(out_img, contours, filled=False):
@@ -170,8 +182,7 @@ def draw_bounding_rect(out_img, contours, filled=False):
     for c in contours:
         # Now draw bounding box
         x, y, w, h = cv2.boundingRect(c)
-        thickness = -1 if filled else 2
-        cv2.rectangle(out_img,(x,y),(x+w,y+h),(0,255,0), thickness)
+        cv2.rectangle(out_img,(x,y),(x+w,y+h),(0,255,0), 2)
 
 def draw_bounding_circle(out_img, contours):
 
@@ -182,21 +193,61 @@ def draw_bounding_circle(out_img, contours):
         radius = int(radius)
         cv2.circle(out_img, center, radius, (0, 0, 255), 3)
 
-#def use_match_detection(frame, template):
-#     gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)q
-#     template = cv2.imread(template, 0)
-#     w, h = template.shape[::-1]
-#
-#     res = cv2.matchTemplate(gray_frame,template,cv2.TM_CCOEFF_NORMED)
-#     threshold = 0.8
-#     loc = np.where(res >= threshold)
-#
-#     cp_frame = frame.copy()
-#
-#     for pt in zip(*loc[::-1]):
-#         cv2.rectangle(cp_frame, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-#
-#     # cv2.imshow('webcam', cp_frame)
+def draw_the_future(out_img, future_pairs):
+
+    print "I want to draw %s " % future_pairs
+    for pair in future_pairs:
+        cv2.line(out_img, pair[0], pair[1], (0, 0, 255), 3)
+
+def find_shortest_distances(curr_cnts, prev_cnts):
+    '''Naively assume that for a single coord, shortest distance
+    will be a 1-to-1 for all in both curr set and prev set.'''
+    master_set = []
+
+    def center(cnt):
+        M = cv2.moments(cnt)
+        centroid_x = int(M['m10']/M['m00'])
+        centroid_y = int(M['m01']/M['m00'])
+        return centroid_x, centroid_y
+
+    curr = [center(contour) for contour in curr_cnts]
+    prev_coords = [center(contour) for contour in prev_cnts]
+
+    for x,y in curr:
+        print "X:%s,Y:%s, prev:%s" % (x, y, prev_coords)
+        best_dist = maxint
+        best_pair = None
+
+        for p_x, p_y in prev_coords:
+            print "P_x: %s, p_y: %s" % (p_x, p_y)
+            dist = sqrt((x-p_x)**2 + (y-p_y)**2)
+            print "Dist: %s" % dist
+            if dist < best_dist:
+                best_dist = dist
+                best_pair = p_x, p_y
+        #Associate current x,y to optimal distance pair
+        master_set.append([best_pair, (x,y)])
+
+    return master_set
+
+
+def anticipate_the_future(matched_pairs):
+
+    print "Matched: %s" % matched_pairs
+    future_locs = []
+
+    for coord_set in matched_pairs:
+
+        first_x, first_y = coord_set[0]
+        sec_x, sec_y = coord_set[1]
+
+        new_x = first_x + 2*(sec_x-first_x)
+        new_y = first_y + 2*(sec_y-first_y)
+
+        future_locs.append([(sec_x,sec_y), (new_x, new_y)])
+
+    print "Future %s " % future_locs
+    return future_locs
 
 if __name__ == '__main__':
         main()
