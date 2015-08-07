14e6409a18af7778e3f3d44c062902665848709c
Alvaro Prieto <apg88zx@gmail.com>
2015-08-06 19:03:27 -0700

    Adding galvoController class to keep all laser shooting stuff in one place

--- sw/galvoVision/simpleClickTest.py
+++ sw/galvoVision/simpleClickTest.py
@@ -3,20 +3,17 @@
 # Read in lookup table, wait for mouse click, choose closest point, shoot laser
 # Currently using 50x50 lookup table, which takes a while to read in
 # 
-import argparse
-import cv2
+from galvoController import galvoController
 import numpy as np
-import math
-import serial
 import threading
-import timeit
-import csv
+import argparse
+import math
 import time
-import copy
-import os
+import cv2
+import csv
 import sys
+import os
 
-dotTable = []
 laserX = 0
 laserY = 0
 
@@ -25,21 +22,6 @@ imgBounds = (0,0,1920,1080)
 
 def nothing(x):
     pass
-
-class serialReadThread(threading.Thread):
-    def __init__(self, inStream):
-        super(serialReadThread, self).__init__()
-        self.stream = inStream
-        self.running = 1
-
-    def run(self):
-        while self.running:
-            try:
-                line = self.stream.readline(50)
-                if line:
-                    print line
-            except serial.SerialException:
-                print "serial error"
 
 class cameraReadThread(threading.Thread):
     def __init__(self, cam):
@@ -111,135 +93,23 @@ def getPointBounds(pointList, frame = (0,0,1920,1080), margin = 0):
 
     return (minX, minY, maxX, maxY)
 
-def setLaserState(state):
-    if(state):
-        stream.write('laser 1\n')
-    else:
-        stream.write('laser 0\n')
-
-def setLaserPos(x, y):
-    stream.write("g 0 " + str(x) + "\n")
-    stream.write("g 1 " + str(y) + "\n")
-
-def laserShoot(target = '*', id = '00'):
-    stream.write('s [' + target + 'I' + id + ']\n')
-
 def mouseClick(event, x, y, flags, param):
     global laserX
     global laserY
     global imgBounds
+    global controller
     
     if event == cv2.EVENT_LBUTTONDOWN:
         print("Mouse clicked(" + str(x) + ", " + str(y) +")")
         x += imgBounds[0]
         y += imgBounds[1]
         print("Adjusted for frame(" + str(x) + ", " + str(y) +")")
-        laserPoint = getLaserPos(x, y, 1)
+        laserPoint = controller.getLaserPos(x, y)
         laserX = laserPoint[0]
         laserY = laserPoint[1]
-        setLaserPos(laserX, laserY)
+        controller.setLaserPos(laserX, laserY)
         time.sleep(0.005)
-        laserShoot()
-
-def drawPoints(pointList, img, size = 1, color = [0, 255, 0]):
-    for point in pointList:
-        matchingPoint = (point[2], point[3])
-        cv2.circle(img, matchingPoint, size, color)
-
-def addMidPoints(pointList, img, dupeThreshold = 5):
-    newPointList = copy.copy(pointList)
-    for p1 in pointList:
-        for p2 in pointList:
-            
-            # Remove complete duplicates
-            if p1 == p2:
-                continue
-
-            midPoint = getMidPoint((p1[2], p1[3]), (p2[2], p2[3]))
-            midLaserPoint = getMidPoint((p1[0], p1[1]), (p2[0], p2[1]))
-            
-            # Check against all other points, if they are within dupeThreshold, don't add the new one
-            dist = 1e99
-            for p3 in newPointList:
-                 dist = getDist((midPoint[0], midPoint[1]), (p3[2], p3[3]))
-                 if dist < dupeThreshold:
-                    break
-
-            # This is a new point, add it!
-            if dist >= dupeThreshold:
-                newPointList.append([midLaserPoint[0], midLaserPoint[1], midPoint[0], midPoint[1]])
-
-    return newPointList
-
-def getMidPoint(p1, p2):
-    return int((p1[0] + p2[0])/2), int((p1[1] + p2[1])/2)
-
-def getDist(p1, p2):
-    return math.sqrt(math.pow((p1[0] - p2[0]),2) + math.pow((p1[1] - p2[1]),2))
-
-def getClosestPoints(pointList, point, nPoints = 4):
-    distTable = []
-
-    pixelX = point[0]
-    pixelY = point[1]
-
-    # 
-    # Get distance from every calibration point to pixelX, pixelY
-    # 
-    for index in range(len(pointList)):
-        dotX = pointList[index][2]
-        dotY = pointList[index][3]
-
-        # Don't include if it's an exact match
-        # if dotX == pixelX and dotY == pixelY:
-            # continue
-
-        dist = getDist((pixelX, pixelY), (dotX, dotY))
-        distTable.append([index, dist])    
-
-    sortedDistTable = sorted(distTable, key = lambda x:x[1])
-
-    newTable = []
-    for item in sortedDistTable[0:nPoints]:
-        newTable.append(pointList[item[0]])
-    
-    return newTable
-
-def getLaserPos(pixelX, pixelY, img = None):
-    # Get the four closest points
-    points = getClosestPoints(dotTable, (pixelX, pixelY), 4)
-    # drawPoints(points, img, 5, [0,128,0])
-
-    # Interpolate between them to get new mid-points (with mid laser values calculated too)
-    points = addMidPoints(points, img)
-    # drawPoints(points, img, 3, [0,128,0])
-
-    # Repeat with the new list (including virtual points)
-    points = getClosestPoints(points, (pixelX, pixelY), 4)
-    # drawPoints(points, img, 3, [0,0,128])
-
-    # Add more virtual points!
-    points = addMidPoints(points, img)
-    # drawPoints(points, img, 2, [0,0,128])
-
-    # And again...
-    points = getClosestPoints(points, (pixelX, pixelY), 4)
-    # drawPoints(points, img, 2, [0,128,128])
-
-    points = addMidPoints(points, img, dupeThreshold = 4)
-    # drawPoints(points, img, 1, [0,128,128])
-
-    # and again
-    points = getClosestPoints(points, (pixelX, pixelY), 4)
-    # drawPoints(points, img, 1, [0,255,0])
-
-    points = addMidPoints(points, img, dupeThreshold = 2)
-    # drawPoints(points, img, 1, [0,255,0])
-
-    # Get final closest point
-    points = getClosestPoints(points, (pixelX, pixelY), 1)
-    return points[0]
-
+        controller.laserShoot()
 
 # 
 # Start Here!
@@ -251,28 +121,13 @@ if len(sys.argv) < 2:
 
 streamFileName = sys.argv[1]
 
-stream = serial.Serial(streamFileName)
-
-# Start readThread as daemon so it will automatically close on program exit
-readThread = serialReadThread(stream)
-readThread.daemon = True
-readThread.start()
-
-# 
-# Read table into list of lists (i think)
-# Format is "laserX, laserY, pixelX, pixelY"
-# 
-with open('dotTable.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        # Ignore bad values
-        if int(row[2]) > 0 and int(row[3]) > 0:
-            dotTable.append([int(row[0]), int(row[1]), int(row[2]), int(row[3])])
+controller = galvoController(streamFileName)
+controller.loadDotTable('dotTable.csv')
 
 # Get image margins from dotTable
 # No need to display what we can't shoot
 # TODO - pass as parameter?
-imgBounds = getPointBounds(dotTable, margin=25)
+imgBounds = getPointBounds(controller.dotTable, margin=25)
 
 print('New image bounds: (' + str(imgBounds[0]) + ',' +str(imgBounds[1]) + ',' +str(imgBounds[2]) + ',' + str(imgBounds[3]) + ')')
 
@@ -288,7 +143,7 @@ running = True
 
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", mouseClick)
-setLaserState(True)
+controller.setLaserState(True)
 while running:
     image = cameraThread.getFrame()
     # cv2.circle(image, (laserX, laserY), 5, [0,255,0])
@@ -298,6 +153,6 @@ while running:
     if k == 27:
         running = False
 
-setLaserState(False)
+controller.setLaserState(False)
 
 time.sleep(0.100)
